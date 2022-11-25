1edad7bd89218d7046cb995be722e335be06656a
marta-santos <martasilvaa325@gmail.com>
2022-11-25 17:22:43 -0300

    mudanças no css global e no portfolio

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -8,12 +8,6 @@ export function Portfolio() {
             <h2 className="subititulo2">Meus projetos favoritos</h2>
             <div className="divportfolio">
                 <div className="item">
-                    <h3>Lugar de Mulher</h3>
-                    <p>Projeto sobre mulheres na tecnologia</p>
-                    <p>Gradys Weste foi a expiração</p>
-                    <a className="direciona" href="https://amazing-pithivier-2a8d40.netlify.app/" target="_blank">Ir para pagina</a>
-                </div>
-                <div className="item">
                     <h3>To-Do list</h3>
                     <p>Para marcar afazeres</p>
                     <a className="direciona" href="https://tubular-kitten-16d9e6.netlify.app/" target="_blank" >Ir para pagina</a>
@@ -28,6 +22,12 @@ export function Portfolio() {
                     <p>Meu segundo projeto React</p>
                     <p>Esse é um dos que mais gosto!</p>
                     <a className="direciona" href="https://segundo-projeto-react.vercel.app/" target="_blank" >Ir Para pagian</a>
+                </div>
+                <div className="item">
+                    <h3>Lugar de Mulher</h3>
+                    <p>Mulheres na tecnologia</p>
+                    <p>Gradys Weste foi a expiração</p>
+                    <a className="direciona" href="https://amazing-pithivier-2a8d40.netlify.app/" target="_blank">Ir para pagina</a>
                 </div>
             </div>
         </>
